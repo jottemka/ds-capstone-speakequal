bafa471503aa12dd8c9899769dc21532b58e9d1b
jottemka <jakob.koscholke@gmx.de>
2024-09-02 22:50:39 +0200

    Autostart browser for web-based app.

--- scripts/diart_websocket.py
+++ scripts/diart_websocket.py
@@ -3,13 +3,14 @@ from diart.sources import WebSocketAudioSource
 from diart.inference import StreamingInference
 from diart.models import SegmentationModel, EmbeddingModel
 from diart.sinks import Observer
+import webbrowser
 
 from huggingface_hub import login
 
 import json
 
 SAMPLE_RATE = 16000
-HUGGING_FACE_TOKEN = "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"
+#HUGGING_FACE_TOKEN = "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"
 
 class DummyObserver(Observer):
 
@@ -53,5 +54,9 @@ inference = StreamingInference(
 inference.attach_hooks(client_response)
 inference.attach_observers(DummyObserver())
 
+webbrowser.open('../webapp/streaming.html')
+
 print("Waiting for signal..")
 prediction = inference()
+
+
